6987e29e120001ffebb31f59b85787296b8b40c7
e <wlx100715@163.com>
2019-02-17 22:43:53 +0800

    Delete extra code.

--- Java/chicken/src/service/BuyChicken.java
+++ Java/chicken/src/service/BuyChicken.java
@@ -13,9 +13,6 @@ public class BuyChicken {
         for (int i = 0; i <= 100 / cock.getPrice(); i++) {
             for (int j = 0; j <= 100 / hen.getPrice(); j++) {
                 int k = 100 - i - j;
-                if (k < 0) {
-                    continue;
-                }
                 if (i * cock.getPrice() + j * hen.getPrice() + k * chick.getPrice() == 100.00) {
                     System.out.println("买" + i + "只公鸡，" + j + "只母鸡，" + k + "只小鸡");
                 }
