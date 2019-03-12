1d78753aac9dbae1d593f5ce95dd9ac9601267d7
e <wlx100715@163.com>
2019-03-12 19:57:57 +0800

    Add the practice of Optional.

---
+++ Java/optional/Commodity.java
@@ -0,0 +1,37 @@
+package optional;
+
+public class Commodity {
+    private int id;
+    private String name;
+    private int price;
+
+    public Commodity(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
